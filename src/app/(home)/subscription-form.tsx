dd4db17014581c6a66648d4f087a6307fafd7621
NattanSilva <natanxboy@gmail.com>
2025-03-22 20:08:28 -0300

    aula 3

--- src/app/(home)/subscription-form.tsx
+++ src/app/(home)/subscription-form.tsx
@@ -2,8 +2,10 @@
 
 import { Button } from '@/components/button'
 import { InputField, InputIcon, InputRoot } from '@/components/input'
+import { postSubscriptions } from '@/http/api'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { ArrowRight, Mail, User } from 'lucide-react'
+import { useRouter, useSearchParams } from 'next/navigation'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 
@@ -15,6 +17,9 @@ const subscriptioSchema = z.object({
 type SubscriptionSchema = z.infer<typeof subscriptioSchema>
 
 export function SubscriptionForm() {
+  const router = useRouter()
+  const searchParams = useSearchParams()
+
   const {
     register,
     handleSubmit,
@@ -23,8 +28,11 @@ export function SubscriptionForm() {
     resolver: zodResolver(subscriptioSchema),
   })
 
-  function onSubscribe(data: SubscriptionSchema) {
-    console.log(data)
+  async function onSubscribe({ name, email }: SubscriptionSchema) {
+    const referrer = searchParams.get('referrer')
+    const { subscriberId } = await postSubscriptions({ name, email, referrer })
+
+    router.push(`/invite/${subscriberId}`)
   }
 
   return (
